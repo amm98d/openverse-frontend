ea0cca3428973ccf6732bec67dd5f93df5ab78ca
Olga Bulat <obulat@gmail.com>
2021-11-16 17:08:10 +0300

    Fix UI when query q is not set
    Remove logging

--- src/pages/search.vue
+++ src/pages/search.vue
@@ -27,6 +27,7 @@
               :fetch-state="fetchState"
               :is-finished="isFinished"
               :is-filter-visible="isFilterVisible"
+              :search-term="query.q"
               @load-more="fetchMedia"
             />
           </template>
@@ -78,7 +79,11 @@ const BrowsePage = {
   },
   scrollToTop: false,
   async fetch() {
-    if (this.mediaType !== VIDEO && this.results.items.length === 0) {
+    if (
+      this.mediaType !== VIDEO &&
+      this.results.items.length === 0 &&
+      this.query.q.trim() !== ''
+    ) {
       await this.fetchMedia({ mediaType: this.mediaType, q: this.query.q })
     }
   },
@@ -142,7 +147,9 @@ const BrowsePage = {
       setFilterVisibility: SET_FILTER_IS_VISIBLE,
     }),
     async getMediaItems(params, mediaType) {
-      await this.fetchMedia({ ...params, mediaType })
+      if (params.q.trim() !== '') {
+        await this.fetchMedia({ ...params, mediaType })
+      }
     },
     onLoadMoreItems(searchParams) {
       this.getMediaItems(searchParams, this.mediaType)
