5b11e7b2ac5dbd415e0fe04415d2340936f783fb
Olga Bulat <obulat@gmail.com>
2021-12-06 20:15:20 +0300

    Add VHeader stub

---
+++ src/components/VHeader/VHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-white w-full z-20 fixed">
+    <VFilterButton @toggle="toggleFilter" />
+  </div>
+</template>
+
+<script>
+import VFilterButton from '~/components/VHeader/VFilterButton'
+import { defineComponent } from '@nuxtjs/composition-api'
+
+const VHeader = defineComponent({
+  name: 'VHeader',
+  components: {
+    VFilterButton,
+  },
+})
+
+export default VHeader
+</script>
